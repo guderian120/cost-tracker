6e8839b8e55bfe9595eb377aa7c7803e253c56ac
Andy Amponsah <42794895+guderian120@users.noreply.github.com>
2025-08-19 14:21:12 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const { pool } = requre('./db');
+const { pool } = require('./db');
 require('dotenv').config();
 const app = express();
 app.use(cors());
